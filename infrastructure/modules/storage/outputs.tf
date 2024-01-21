99b694829f28c49ba33fbdec16be7196aafe990a
vasil dhima <vdhimas@gmail.com>
2024-01-21 14:31:36 +0100

    fix

--- infrastructure/modules/storage/outputs.tf
+++ infrastructure/modules/storage/outputs.tf
@@ -1,11 +1,11 @@
 output "web-client-domain-name" {
-  value = aws_s3_bucket.web-client-bucket.bucket_domain_name
+  value = aws_s3_bucket.my-static-website.bucket_domain_name
 }
 
 output "web-client-bucket-name" {
-  value = aws_s3_bucket.web-client-bucket.id
+  value = aws_s3_bucket.my-static-website.id
 }
 
 output "web-client-website-domain" {
-  value = aws_s3_bucket_website_configuration.web-client-website.website_domain
+  value = aws_s3_bucket_website_configuration.my-static-website-config.website_domain
 }
